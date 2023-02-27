0cf89983f497420013c55f836db2305c24c8d9a9
Tony Castrogiovanni <afarentino@gmail.com>
2023-02-27 12:27:14 -0500

    Initial commit

---
+++ src/test/java/com/github/afarentino/datepicker/DatepickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.afarentino.datepicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatepickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
